c61efc02ecaca1c62ed282d1d0a8b34f321f429f
张嘉夫 <zhangkuan523@icloud.com>
2016-12-29 09:50:38 +0800

    Initial Commit

---
+++ MenuBarNBA/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuBarNBA
+//
+//  Created by 张嘉夫 on 16/12/29.
+//  Copyright © 2016年 张嘉夫. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
